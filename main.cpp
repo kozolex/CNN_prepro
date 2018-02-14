d96d59c01d07832ec4a33d7ed140303a5c803c36
kozolex <kozolex@gmail.com>
2018-02-14 22:25:20 +0100

    0.4 Serching in folder

--- main.cpp
+++ main.cpp
@@ -2,37 +2,79 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
 int main()
 {
+    string tabDir[20];
+    string _dir = "F:\\noweFotyZiaren\\NormaOdmiany\\";
+    const char * _dir_cch = _dir.c_str();
+    struct dirent * plik;
+    DIR * sciezka;
+    int i=0;
+    if(( sciezka = opendir( _dir_cch ) ) )
+    {
+
+        while(( plik = readdir( sciezka ) ) )
+        {
+            tabDir[i]=plik->d_name;
+            i++;
+        }
+        closedir( sciezka );
+    }
+    else
+        cout <<" b³¹d otwarcia strumienia dla danej œcie¿ki, mo¿e nie istnieje, lub podano œcie¿kê pust¹\n";
+
+    string _filtr = "*.tif";
+
+    //for (int j=2; j<i; j++) cout<<tabDir[j]<<endl;
+
+
+
     Mat imageSrc;
     int maxWidth =0, maxHeight =0;
-    string dirStr = "F:\\noweFotyZiaren\\NormaOdmiany\\JOVincenta\\";
-    const char * dirSrc = dirStr.c_str();
-
-    DIR* katalog = opendir(dirSrc); //<sciezka> zamieniamy na œcie¿kê do katalogu
-    struct dirent* pozycja=0;
-    vector<string> nazwa;
-    string plik;
-    while (pozycja=readdir(katalog))
+    for (int j=2; j<i; j++)
     {
-        plik=(*pozycja).d_name;
-        if (plik.find(".tif")==(plik.length()-4))   //.txt mozna zamienic na dowolne 4 znaki znajdujace sie na koncu nazwy
+
+
+        string dirStr = _dir+tabDir[j]+"\\";
+        //cout<<dirStr<<endl<<endl;
+        const char * dirSrc = dirStr.c_str();
+
+        DIR* katalog = opendir(dirSrc); //<sciezka> zamieniamy na œcie¿kê do katalogu
+        struct dirent* pozycja=0;
+        vector<string> nazwa;
+        string plik;
+        while (pozycja=readdir(katalog))
         {
-            imageSrc= imread(dirStr + plik);
-            if(maxWidth < imageSrc.cols) maxWidth=imageSrc.cols;
-            if(maxHeight < imageSrc.rows) maxHeight=imageSrc.rows;
-            //plik.erase(plik.length()-4); //usuwanie .txt
-            //cout<<plik<<endl;
-            cout<<".";
+            plik=(*pozycja).d_name;
+            if (plik.find(".tif")==(plik.length()-4))   //.txt mozna zamienic na dowolne 4 znaki znajdujace sie na koncu nazwy
+            {
+                imageSrc= imread(dirStr + plik);
+                if(maxWidth < imageSrc.cols)
+                    maxWidth=imageSrc.cols;
+                if(maxHeight < imageSrc.rows)
+                    maxHeight=imageSrc.rows;
+                //plik.erase(plik.length()-4); //usuwanie .txt
+                //cout<<plik<<endl;
+                //cout<<".";
+            }
+
         }
-
+        //cout<<dirStr + plik<<"*"<<endl;
+        cout<<endl<<tabDir[j]<<"->"<<"MAX width: "<<maxWidth<<" MAX heigth: "<<maxHeight<<endl;
+        maxWidth=0;
+        maxHeight=0;
+        closedir(katalog);
     }
-    cout<<endl<<"MAX width: "<<maxWidth<<endl<<"MAX heigth: "<<maxHeight<<endl;
+    //cout<<endl<<"MAX width: "<<maxWidth<<" MAX heigth: "<<maxHeight<<endl;
 
-    closedir(katalog);
+
+
+
     return 0;
 }
