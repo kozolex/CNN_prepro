f3e786299701aa826b6e0c96d444c40895853991
kozolex <kozolex@gmail.com>
2018-02-14 20:30:53 +0100

    0.1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
