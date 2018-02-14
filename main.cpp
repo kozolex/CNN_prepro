3d093d0f6e725b0087173e73f2f54c25eea36cfb
kozolex <kozolex@gmail.com>
2018-02-14 21:28:23 +0100

    0.3 Find the biggest size

--- main.cpp
+++ main.cpp
@@ -8,7 +8,12 @@ using namespace std;
 
 int main()
 {
-    DIR* katalog = opendir( "F:\\noweFotyZiaren\\NormaOdmiany\\Bordo2016" ); //<sciezka> zamieniamy na œcie¿kê do katalogu
+    Mat imageSrc;
+    int maxWidth =0, maxHeight =0;
+    string dirStr = "F:\\noweFotyZiaren\\NormaOdmiany\\JOVincenta\\";
+    const char * dirSrc = dirStr.c_str();
+
+    DIR* katalog = opendir(dirSrc); //<sciezka> zamieniamy na œcie¿kê do katalogu
     struct dirent* pozycja=0;
     vector<string> nazwa;
     string plik;
@@ -17,10 +22,16 @@ int main()
         plik=(*pozycja).d_name;
         if (plik.find(".tif")==(plik.length()-4))   //.txt mozna zamienic na dowolne 4 znaki znajdujace sie na koncu nazwy
         {
+            imageSrc= imread(dirStr + plik);
+            if(maxWidth < imageSrc.cols) maxWidth=imageSrc.cols;
+            if(maxHeight < imageSrc.rows) maxHeight=imageSrc.rows;
             //plik.erase(plik.length()-4); //usuwanie .txt
-            cout<<plik<<endl;
+            //cout<<plik<<endl;
+            cout<<".";
         }
+
     }
+    cout<<endl<<"MAX width: "<<maxWidth<<endl<<"MAX heigth: "<<maxHeight<<endl;
 
     closedir(katalog);
     return 0;
